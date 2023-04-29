9fe9c635c0423bd1aab42e0509089b5323667a52
Jonas Hillenbrand <hillenbrandjonas@hotmail.com>
2023-04-29 15:04:42 +0200

    removed optimization part

--- src/net/sytes/botg/array/optimization/IAlgorithm.java
+++
@@ -1,7 +0,0 @@
-package net.sytes.botg.array.optimization;
-
-public interface IAlgorithm {
-
-	public void run(double[][] X_ij);
-	
-}
--- src/net/sytes/botg/array/optimization/IObjectFunction.java
+++
@@ -1,9 +0,0 @@
-package net.sytes.botg.array.optimization;
-
-public interface IObjectFunction {
-
-	public double apply(double[] x_j);
-	
-	public double apply(double[][] X_ij);
-		
-}
